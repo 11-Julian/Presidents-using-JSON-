25a54ae8bb1fba0ae146a2717d5d246d0faeec98
11-Julian <11julian99@gmail.com>
2024-04-03 14:06:39 -0500

    Initial Commit

---
+++ President/PresidentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresidentApp.swift
+//  President
+//
+//  Created by Julian on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PresidentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
